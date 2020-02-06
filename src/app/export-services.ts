52bdad2f03dd0524da387c2d969a520184712b8b
ianvelazquez <lestrada@ax.opticalux.com.mx>
2020-02-06 09:21:59 -0600

    conflicts fixed

--- src/app/export-services.ts
+++ src/app/export-services.ts
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import {LoginService} from './services/login.service';
 
 
 export const ServicesArray = [
     LoginService
 ]
-=======
-import {LoginService} from './services/login/login.service';
-
-export const ExportServices = [
-    LoginService
-];
->>>>>>> c191ad9b7444345a72fe15b80a090fe3b4ae9fed
